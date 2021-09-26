e0a96258d211be5f9eefcf049bbd0489c94faf74
Noonland1988 <86056308+Noonland1988@users.noreply.github.com>
2021-09-27 06:58:42 +0900

    Initial Commit

---
+++ OnTheMapUITests/OnTheMapUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  OnTheMapUITestsLaunchTests.swift
+//  OnTheMapUITests
+//
+//  Created by 嶋田省吾 on 2021/09/27.
+//
+
+import XCTest
+
+class OnTheMapUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
